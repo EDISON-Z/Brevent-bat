0032f92424c75608167706673474da57a1df601e
EDISON,Z <zuohaozheng@hotmail.com>
2017-06-06 00:42:36 +0900

    Update brevent.bat
    Auto Requesting administrative privileges
    自动请求管理员权限

--- brevent.bat
+++ brevent.bat
@@ -1,27 +1,44 @@
-cd C:\Program Files (x86)\Android\android-sdk\platform-tools
-echo Designed By Edison.Z
-echo Github:https://github.com/EDISON-Z/Brevent-bat
-
 @echo off
-goto check_Permissions
+title Brevent-bat By Edison.Z
+COLOR 03
+echo Brevent-bat By Edison.Z
+echo Github:https://github.com/EDISON-Z/Brevent-bat
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
 
-:check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-	echo ĞèÒª¹ÜÀíÔ±È¨ÏŞ¡£ÕıÔÚ¼ì²â¹ÜÀíÔ±È¨ÏŞ£¬ÇëÄÍĞÄµÈ´ı......
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    echo æ­£åœ¨è¯·æ±‚ç®¡ç†å‘˜æƒé™...
+    goto UACPrompt
+) else ( goto gotAdmin )
 
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-		echo Success: Administrative permissions confirmed.
-		echo ³É¹¦£º¹ÜÀíÔ±È¨ÏŞ¿ÉÓÃ
-		echo ÇëÁ¬½ÓÉè±¸£¬²¢¿ªÆôUSBµ÷ÊÔ
-			pause
-		adb devices
-			pause
-		adb -d shell sh /sdcard/Android/data/me.piebridge.brevent/brevent.sh
-		pause
-    ) else (
-		echo ^G ÇëÒÔ¹ÜÀíÔ±È¨ÏŞÔËĞĞ 
-    )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
-    pause >nul
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
 
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+echo Please connect your devices, and make sure you are already enable USB Debug Mode
+echo è¯·è¿æ¥è®¾å¤‡ï¼Œå¹¶å¼€å¯USBè°ƒè¯•
+pause
+
+adb devices
+pause
+
+adb devices
+adb -d shell sh /sdcard/Android/data/me.piebridge.brevent/brevent.sh
+pause
