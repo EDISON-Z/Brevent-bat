cbab494c9c58c19dc15c4806d5c279cf7a6d19c6
EDISON,Z <zuohaozheng@hotmail.com>
2017-07-21 12:10:29 +0800

    Update brevent.bat

--- brevent.bat
+++ brevent.bat
@@ -1,10 +1,3 @@
-@echo off
-title Brevent-bat By Edison.Z
-echo Brevent-bat By Edison.Z
-echo Github:https://github.com/EDISON-Z/Brevent-bat
-echo Please connect your devices, and make sure you are already enable USB Debug Mode
-echo 请连接设备，并开启USB调试
-pause
 adb devices
 pause
 adb devices
